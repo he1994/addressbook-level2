9d4c2a11bf8bfccbb544635b7b8032a30449a9c7
PLUS\Jing <jinghe2105@gmail.com>
2016-09-08 10:42:01 +0800

    Fix indent and add comments

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,10 @@
 package seedu.addressbook.data.person;
 
+/**
+ * 
+ * Print the details in a specific format
+ *
+ */
 public interface Printable {
-	String getPrintableString();
+    String getPrintableString();
 }
